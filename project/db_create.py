0795c93a4d95b4fdc43b65d91eb027898f139d63
Danny Wood <dannywood1976@gmail.com>
2020-04-09 19:02:55 +0100

    Completion of User class

--- project/db_create.py
+++ project/db_create.py
@@ -4,7 +4,7 @@ from datetime import date
 
 db.create_all()
 
-db.session.add(Task("Finish this tutorial", date(2020, 9, 22), 10, 1))
-db.session.add(Task("Finish Real Python", date(2020, 10, 3), 10, 1))
+#db.session.add(Task("Finish this tutorial", date(2020, 9, 22), 10, 1))
+#db.session.add(Task("Finish Real Python", date(2020, 10, 3), 10, 1))
 
 db.session.commit()
